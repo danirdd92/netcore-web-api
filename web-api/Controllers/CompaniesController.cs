5211490a6cce255ff8820fa069e1ccf9e5100858
Dani Rashba <danirdd92@gmail.com>
2020-09-03 18:10:46 +0300

    Added GET requests

--- web-api/Controllers/CompaniesController.cs
+++ web-api/Controllers/CompaniesController.cs
@@ -29,18 +29,25 @@ namespace web_api.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
-                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
-                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
-                return Ok(companiesDto);
+            return Ok(companiesDto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if (company is null)
+            {
+                _logger.LogInfo($"Company with id: {id} doesn't exist in the database.");
+                return NotFound();
             }
-            catch (Exception ex)
+            else
             {
-                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
-
-                return StatusCode(500, "Internal server error");
+                var companyDto = _mapper.Map<CompanyDto>(company);
+                return Ok(companyDto);
             }
         }
     }
